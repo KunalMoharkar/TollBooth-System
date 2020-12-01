252350d6ade0321a1b4edcc9c22a61a48dc01bdf
Kunal Moharkar <kunalmoharkar1700@gmail.com>
2020-12-01 23:48:39 +0530

    Driver code

--- src/TollBoothApplication.java
+++ src/TollBoothApplication.java
@@ -4,17 +4,36 @@ import java.util.ArrayList;
 public class TollBoothApplication {
 	
 	public static void main(String[] args)
-	{
+	{	
+		
+		System.out.println("");
+		
 		Barcode b1 = new Barcode("1",5);
 		Truck t1 = new TataTruck(b1, 12500);
 		
 		TollBooth booth = new HighwayBooth();
 		
-		Receipt rec = booth.generateReceipt(t1);
 		
+		System.out.println("Receipt Genetration");
+		
+		Receipt rec = booth.generateReceipt(t1);
 		rec.showReceipt();
 		
+		System.out.println("Update booth status");
 		
+	
+		booth.showBoothStats();
+		
+		System.out.println("With draw cash");
+		
+		booth.collectReceipts();
+		
+		System.out.println("reset booth status");
+		
+		booth.showBoothStats();
+		
+		
+		System.out.println("output demo for 5th point (date query)");
 		
 		ArrayList<Receipt> receiptList = booth.showBoothEntries("15-04-2020", "10-10-2020");
 		
